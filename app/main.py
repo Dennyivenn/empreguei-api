8ae423321df450f98c1f53ac82940a29b6c7e414
Denny ivenn <dennyivenn@gmail.com>
2025-06-01 14:41:02 +0200

    Corrigir main.py e estrutura para rodar no Render

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
-uvicorn app.main:app --host=0.0.0.0 --port=$PORT
 from fastapi import FastAPI
 from app.routers import users, jobs, applications
 
 app = FastAPI()
 
-# Incluindo as rotas
 app.include_router(users.router)
 app.include_router(jobs.router)
 app.include_router(applications.router)
